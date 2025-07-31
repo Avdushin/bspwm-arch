7743f74ef86281e5ea0ec83a5aee327a079d979f
Avdushin <itsdobro@gmail.com>
2025-07-31 20:35:50 +0300

    Legooo

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,36 +1,38 @@
-# aliases
-#sh $HOME/bin/fetchs/fetch
+
+# Create aliases
 alias cls="clear"
 alias g="git"
 alias n="nvim"
+alias v="vin"
 alias m="micro"
-alias md="mkdir"
-alias ls="lsd"
-#alias history="history | cat -n"
-alias h="history | cat -n"
 
-# monitoring
-alias psf="procstats"
-alias prs="procstats"
-# proc stats pool
-alias topcpu="topprocs --cpu"
-alias topmem="topprocs --mem"
+# Display critical errors
+alias syslog_emerg="sudo dmesg --level=emerg,alert,crit"
 
-# Spirited away Aburya wallpapers
-alias wp="feh --bg-fill  /home/user/Images/walls.png"
+# Output common errors
+alias syslog="sudo dmesg --level=err,warn"
 
-# fetches
+# Print logs from x server
+alias xlog='grep "(EE)\|(WW)\|error\|failed" ~/.local/share/xorg/Xorg.0.log'
+
+# Remove archived journal files until the disk space they use falls below 100M
+alias vacuum="journalctl --vacuum-size=100M"
+
+# Make all journal files contain no data older than 2 weeks
+alias vacuum_time="journalctl --vacuum-time=2weeks"
+
 alias ff="fastfetch"
 alias fetch="~/bin/fetchs/fetch"
 alias sysfetch="~/bin/fetchs/sysfetch"
 
-# scripts
-alias animals="~/bin/color-scripts/animals"
+# Zapret
+alias zapret="sudo bash ~/zapret-discord-youtube-linux/main_script.sh"
 
 # Setup monitors
 alias setupmon="xrandr --output DP-0 --primary --mode 2560x1440 --rate 165 --pos 0x0 \
                --output DP-4 --mode 1920x1080 --rate 165 --rotate left --pos 2560x0"
 
+# Steam
 alias steam_shutdown='echo "Запуск скрипта выключения после Steam..."; nohup ~/bin/scripts/shutdown_after_steam_install.sh > ~/shutdown.log 2>&1 &; echo "Скрипт запущен в фоне. Логи в ~/shutdown.log"'
 
 set -U fish_greeting
@@ -39,7 +41,6 @@ set -gx EDITOR vim
 set -gx VISUAL vim
 set -gx BROWSER /usr/bin/firefox
 
-set -x DOCKER_HOST unix:///var/run/docker.sock
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
